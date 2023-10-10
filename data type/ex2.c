fbe26a9557366987b502c29ace54925b70ed6a08
Chael_Chael <51958723+Chael-Chael@users.noreply.github.com>
2023-10-10 18:22:36 +0800

    更新 ex2.c

--- data type/ex2.c
+++ data type/ex2.c
@@ -6,7 +6,7 @@ int main(void)
 
     for(int i = 1; i <= 90; i++)
     {
-        printf("[%3d] fn = %25ld\n", i, n1);
+        printf("[%3d] fn = %25lu\n", i, n1);
         temp = n1;
         n1 = n2;
         n2 += temp;
