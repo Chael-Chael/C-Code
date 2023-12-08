77f0e48bee5ecb92b7fa8d3904d54762e6fb1aef
Chael_Chael <2792357190@qq.com>
2023-12-08 21:50:57 +0800

    created

---
+++ text/text_pointer.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_WORD_NUM 200
+#define MAX_WORD_LEN 30
+
+// function to parse the message
+// return the found number of words
+int parseMessage(char message[], char *wordList);
+
+
+int main()
+{
+
+    char message[1000] ;
+    gets(message);
+
+    char wordList[MAX_WORD_NUM][MAX_WORD_LEN] = {NULL};
+    printf("Orignal msg is: %s \n\n", message);
+
+    char *pointer = wordList;
+    int wCounter = parseMessage(message, pointer);
+    printf("\nFind total %d words.\n\n", wCounter);
+
+    return 0;
+}
+
+
+// **********************************************
+// << Your source code starts here >>
+//
+int parseMessage(char message[], char *wordList)
+{
+    int *ptr = message;
+    int counter = 0;
+    while (*ptr != '\0')
+    {
+        if (*ptr == ' ')
+        {
+            counter ++;
+        }
+        else
+        {
+            *wordList = *ptr;
+            wordList++;
+        }
+        ptr ++;
+    }
+}
+//
+// << Your source code stops here >>
+// **********************************************
