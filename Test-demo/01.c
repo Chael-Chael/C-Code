395f960479bae6113986bcf680aabdb5933f6b31
Chael_Chael <2792357190@qq.com>
2024-01-28 20:38:36 +0800

    update

--- Test-demo/01.c
+++ Test-demo/01.c
@@ -20,46 +20,39 @@ void printRects(Cube *p, int *rankArray);
 
 void rankRects(Cube *p, int *rankArray);
 
-void searchRectsByRank(Cube *p, int *rankArraySeq);
+void searchRectsByRank(Cube *p, int *rankArray, int rank);
 
 int main()
 {
     srand(time(NULL));
 
-    //Cube cube1, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10; 
     Cube cuboid[10] = {0};
 
+    // initialize the cuboid and update the volume
     for (int i = 0; i < 10; i++)
     {
         initRect(&cuboid[i]);
         updateRects(&cuboid[i]);
     }
 
+    // rank the cuboid
     int rankArray[10] = {0};
-    int randRank = 1 + rand() % 10;
-
-    printf("randRank by %d\n\n", randRank);\
-
-
-
     rankRects(cuboid, rankArray);
-
-    for (int i = 0; i < 10; i ++)
+    for (int i = 0; i < 10; i++)
     {
-        if (randRank == rankArray[i])
-        {
-            printRects(&cuboid[i], &rankArray[i]);
-            printf("\n");
-        }
+        printRects(cuboid + i, rankArray + i);
     }
 
-    printf("Rank by order\n");
-    searchRectsByRank(cuboid, rankArray);
+    // search the cuboid by rank
+    int rank = 1 + rand() % 10;
+    printf("The %d ranking rect is\n",rank);
+    searchRectsByRank(cuboid, rankArray, rank);
     return 0;
 }
 
 void initRect(Cube *p)
 {
+    // set the name to rectXX
     strcpy(p->name, "rect");
     char digit1 = rand() % 10 + '0';
     char digit2 = rand() % 10 + '0';
@@ -68,9 +61,9 @@ void initRect(Cube *p)
     p->name[5] = digit2;
     p->name[6] = '\0';
 
-    p->length = rand() % 15 + 5;
-    p->width = rand() % 15 + 5;
-    p->height = rand() % 15 + 5;
+    p->length = rand() % 16 + 5;
+    p->width = rand() % 16 + 5;
+    p->height = rand() % 16 + 5;
 }   
 
 void updateRects(Cube *p)
@@ -101,16 +94,20 @@ void printRects(Cube *p, int *rankArraySeq)
     printf("Length:%d\nWidth:%d\nHeight:%d\nVolume:%d\n\n",p->length,p->width,p->height,p->volume);
 }
 
-void searchRectsByRank(Cube *p, int *rankArray)
+void searchRectsByRank(Cube *p, int *rankArray, int rank)
 {
-    for (int i = 1; i < 11; i ++)
+    int isFound = 0;
+    for (int j = 0; j < 10; j ++)
     {
-        for (int j = 0; j < 10; j ++)
+        if (rankArray[j] == rank)
         {
-            if (rankArray[j] == i)
-            {
-                printRects(p+j, rankArray+j);
-            }
+            printRects(p+j, rankArray+j);
+            isFound = 1;
         }
+    }
+    // if no rect is found, print "No rect found"
+    if (isFound == 0)
+    {
+        printf("No rect found\n");
     }
 }
--- Test-demo/02.c
+++ Test-demo/02.c
@@ -16,14 +16,39 @@ int compareBigReal(BigReal *real_1, BigReal *real_2);
 
 void sortBigReals(BigReal *realArray);
 
+void displayBigReal(BigReal *p);
+
 int main()
 {
     srand(time(NULL));
 
     BigReal realArray[6] = {0};
 
-    generateBigReal(BigReal *p);
+    generateBigReal(realArray);
+    printf("Before sort:\n"); 
+    displayBigReal(realArray);
 
+    /*
+    BigReal real_1 = {"123.45600", 3, 6};
+    BigReal real_2 = {"123.456", 3, 6};
+    printf("compare result: %d\n", compareBigReal(&real_1, &real_2));
+    BigReal real_3 = {"12.3456", 3, 6};
+    BigReal real_4 = {"123.456", 3, 6};
+    printf("compare result: %d\n", compareBigReal(&real_3, &real_4));
+    BigReal real_5 = {"123.456", 3, 6};
+    BigReal real_6 = {"123.456", 3, 6};
+    printf("compare result: %d\n", compareBigReal(&real_5, &real_6));
+    BigReal real_7 = {"123.4567", 3, 7};
+    BigReal real_8 = {"123.456", 3, 6};
+    printf("compare result: %d\n", compareBigReal(&real_7, &real_8));
+    BigReal real_9 = {"123.456", 3, 6};
+    BigReal real_10 = {"122.456", 3, 6};
+    printf("compare result: %d\n", compareBigReal(&real_9, &real_10));
+    */
+
+    sortBigReals(realArray);
+    printf("After sort:\n");
+    displayBigReal(realArray);
 
     return 0;
 }
@@ -32,22 +57,29 @@ BigReal *generateBigReal(BigReal *p)
 {
     for (int i = 0; i < 6; i ++)
     {
-        (p+i)->length = 1 + 20 + rand() % 10;
-
-        (p+i)->real = (char*) malloc( (p+i)->length * sizeof(char));
-
+        // set the length to 20-30
+        (p+i)->length = 20 + rand() % 11;
+        // allocate memory for the real number
+        (p+i)->real = (char*) malloc( ( (p+i)->length + 2) * sizeof(char));
+        // set the decimal point to 1-20
         (p+i)->decimal_point = 1 + rand() % 20;
-
+        // set the first digit to 1-9
         (p+i)->real[0] = '1' + rand() % 9;
-        for (int j = 1; j < (p+i)->length; j++)
+        // set the rest digits to 0-9
+        for (int j = 1; j < (p+i)->length + 2; j++)
         {
-            if (j == (p+i)->decimal_point - 1)
+            // set the decimal point
+            if (j == (p+i)->decimal_point)
             {
                 (p+i)->real[j] = '.';
             }
-            else
+            else if (j == (p+i)->length + 1)
             {
+                (p+i)->real[j] = '\0';
+            }
+            else{
                 (p+i)->real[j] = '0' + rand() % 10;
+            
             }
         }
     }
@@ -57,6 +89,7 @@ BigReal *generateBigReal(BigReal *p)
 
 int compareBigReal(BigReal *real_1, BigReal *real_2)
 {
+    // compare the decimal_point of the real number
     if (real_1->decimal_point > real_2->decimal_point)
     {
         return 1;
@@ -67,38 +100,82 @@ int compareBigReal(BigReal *real_1, BigReal *real_2)
     }
     else
     {
-        double num_1 = 0.0, num_2 = 0.0;
-        for (int i = 0; i < real_1->length; i ++)
+        // get the min length of the real number
+        int min_length = real_1->length > real_2->length ? real_2->length : real_1->length;
+        for (int i = 0; i < min_length + 1; i ++)
         {
-            int power = real_1->decimal_point - 2;
-
-            if (i == real_1->decimal_point - 1)
+            if (real_1->real[i] > real_2->real[i])
             {
-                power = -1;
-                continue;
+                return 1;
             }
-
-            num_1 += ((real_1->real[i]) - '0') * (10, power);
-            power --;
+            else if (real_1->real[i] < real_2->real[i])
+            {
+                return -1;
+            }
         }
 
-        for (int j = 0; j < real_2->decimal_point; j ++)
+        if (real_1->length > real_2->length)
         {
-            int power = real_2->decimal_point - 2;
-
-            if (j == real_2->decimal_point - 1)
+            // check if the rest digits are all 0
+            for (int j = min_length + 1; j < real_1->length + 1; j ++)
             {
-                power = -1;
-                continue;
+                if (real_1->real[j] != '0')
+                {
+                    return 1;
+                }
             }
-
-            num_2 += ((real_2->real[i]) - '0') * (10, power);
-            power --;
+            return 0;
+        }
+        else if (real_1->length < real_2->length)
+        {
+            // check if the rest digits are all 0
+            for (int j = min_length + 1; j < real_2->length + 1; j ++)
+            {
+                if (real_2->real[j] != '0')
+                {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+        else
+        {
+            return 0;
         }
     }
 }
 
 void sortBigReals(BigReal *p)
 {
+    int i, j, minIndex;
+    BigReal temp;
+    // selection sort
+    for (i = 0; i < 6 - 1; i++)
+    {
+        minIndex = i;
 
+        for (j = i + 1; j < 6; j++)
+        {
+            if (compareBigReal(p + j, p + minIndex) == -1)
+            {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i)
+        {
+            // swap the real number
+            temp = *(p + i);
+            *(p + i) = *(p + minIndex);
+            *(p + minIndex) = temp;
+        }
+    }
+}
+
+void displayBigReal(BigReal *p)
+{
+    for (int i = 0; i < 6; i ++)
+    {
+        printf("%s\n", (p+i)->real);
+    }
 }
